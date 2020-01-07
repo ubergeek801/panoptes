69a642d948b0339116eb21d24a1f398fca25a658
Jeremy Rosenberger <jlr4@cornell.edu>
2020-01-06 22:48:50 -0700

    eliminating unit test dependency on a Postgres database

---
+++ src/main/java/org/slaq/slaqworx/panoptes/data/JdbcSecurityAttributeLoader.java
@@ -0,0 +1,52 @@
+package org.slaq.slaqworx.panoptes.data;
+
+import javax.inject.Singleton;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowCallbackHandler;
+
+import org.slaq.slaqworx.panoptes.asset.SecurityAttribute;
+
+/**
+ * A service that initializes the known {@code SecurityAttribute}s from a JDBC data source.
+ */
+@Singleton
+public class JdbcSecurityAttributeLoader implements SecurityAttributeLoader {
+    private static final Logger LOG = LoggerFactory.getLogger(JdbcSecurityAttributeLoader.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    /**
+     * Creates a new {@code JdbcSecurityAttributeLoader} that uses the given {@code JdbcTemplate}.
+     * Restricted because this class should be obtained through the {@code ApplicationContext}.
+     *
+     * @param jdbcTemplate
+     *            a {@code JdbcTemplate} from which to obtain data
+     */
+    protected JdbcSecurityAttributeLoader(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void loadSecurityAttributes() {
+        LOG.info("loading {} SecurityAttributes", jdbcTemplate
+                .queryForObject("select count(*) from security_attribute", Integer.class));
+        jdbcTemplate.query("select name, index, type from security_attribute",
+                (RowCallbackHandler)(rs -> {
+                    String name = rs.getString(1);
+                    int index = rs.getInt(2);
+                    String className = rs.getString(3);
+                    Class<?> clazz;
+                    try {
+                        clazz = Class.forName(className);
+                    } catch (ClassNotFoundException e) {
+                        clazz = String.class;
+                        LOG.warn("cannot locate class {} for SecurityAttribute {}", className,
+                                name);
+                    }
+                    SecurityAttribute.of(name, index, clazz);
+                }));
+    }
+}
---
+++ src/test/java/org/slaq/slaqworx/panoptes/data/MockSecurityAttributeLoader.java
@@ -0,0 +1,28 @@
+package org.slaq.slaqworx.panoptes.data;
+
+import javax.inject.Singleton;
+
+import io.micronaut.context.annotation.Primary;
+
+/**
+ * {@code MockSecurityAttributeLoader} is a {code SecurityAttributeLoader} that does nothing
+ * (leaving the default {@code SecurityAttribute}s in place).
+ *
+ * @author jeremy
+ */
+@Singleton
+@Primary
+public class MockSecurityAttributeLoader implements SecurityAttributeLoader {
+    /**
+     * Creates a new {@code MockSecurityAttributeLoader}. Restricted because this class should be
+     * obtained through the {@code ApplicationContext}.
+     */
+    protected MockSecurityAttributeLoader() {
+        // nothing to do
+    }
+
+    @Override
+    public void loadSecurityAttributes() {
+        // nothing to do
+    }
+}
